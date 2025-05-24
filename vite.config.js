aca69f6ad31e255d77e2448cddbd389af30f5024
ASPIRANT AI <155420983+Aspirant-ai@users.noreply.github.com>
2025-05-24 11:50:38 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -184,6 +184,7 @@ logger.error = (msg, options) => {
 export default defineConfig({
 	customLogger: logger,
 	plugins: [react(), addTransformIndexHtml],
+	base: '/MCQgram/',
 	server: {
 		cors: true,
 		headers: {
